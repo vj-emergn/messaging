a01922c0243510986e5a6d8d35716080af063e09
vj-emergn <49640915+vj-emergn@users.noreply.github.com>
2019-06-10 13:26:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ console.log(window.opener);
 const input = document.querySelector('input').addEventListener('input', e => {
   window.opener.postMessage(e.target.value, '*');
 });
+const btn = document.querySelector('button').addEventListener('click', e => {
+  window.opener.postMessage('foo', '*');
+});
